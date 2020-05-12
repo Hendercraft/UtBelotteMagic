0fc42932f6bda8a134bb011d80548cc3287be950
Guillaume <guiguiruff@gmail.com>
2020-05-12 09:18:15 +0200

    Just making some modification to main.c, nothing too big might not  work yet but don't worry, be happy

--- Sources/main.c
+++ Sources/main.c
@@ -2,4 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+	FALSE = 0
+	TRUE = 1
+}Boolean
+
+typedef struct {
+	char value
+	char* color
+	int player
+	Boolean trump 
+{Cards
+
+struct creatcards(char* color){
+	do{ Cards 
+		
+
+
+
+
 int main(){
+
