01eb6a9482a2d8bd2f53f9d343837ab42b744d4a
QuinD3cim <58181290+QuinD3cim@users.noreply.github.com>
2020-05-13 15:44:07 +0200

    Final form
    changed totally the highscore system, cried a few times, then began to comment

--- Sources/highscore.c
+++ Sources/highscore.c
@@ -6,54 +6,38 @@
 
 #include <highscore.h>
 
-/*
- * Checks if the highscore.txt file is present, if not it creates it
- */
-void fileCheck(){
-
-	FILE* file = fopen("highscore.txt","r");
-	if ( file == 0){
-		printf("error, no file found\ncreating file...\n");
-		fflush(stdout);
-		fopen("highscore.txt","w");
-		printf("File created !\n");
-		fflush(stdout);
-	} else {
-		printf("There is a file ! Welcome back !\n");
-		fflush(stdout);
-	}
-	fclose(file);
-
-}
 
 /*
- * Gets and write the name and the score of the person in the file
+ * Gets the name typed by the user, verifies it.
+ * @return a string, the name of the user
  */
-void scoreWrite(int score){
+char* playerCheck(){
 
-	char name[15]="";
-	int i=0,error=0;
-	FILE* file = fopen("highscore.txt","a");
+	char* name = (char*) malloc(15*sizeof(char)); /* allocates a memory of size 15 times the size for a char type for the string 'name'*/
+	strncpy(name,"",15); /* the program initializes the variable 'string' empty */
+	int i = 0;/* initializes the integer i used for the iterations */
+	boolean error = false; /* the boolean error used to tell if there's an error */
 
 	/*Getting and verifying the name*/
-	while(strlen(name) == 0){
-		printf("Enter you name for the score table:\n");
-		fflush(stdout);
-		fgets (name, 15, stdin);
-		while(error == 0 && i<strlen(name)-1){
-			if((0<=name[i] && name[i]<=44) || (47<=name[i] && name[i]<=64) || (91<=name[i] && name[i]<=96) || (123<=name[i] && name[i]<=255)){
-				error = 1;
-				printf("There is a forbidden character in your name : %c\n",name[i]);
-				strncpy(name,"",15);
+		while(strlen(name) == 0){ /* while the length of the string 'name' equals zero: */
+			printf("Enter you name for the score table:\n"); /*asks the user to enter their name;*/
+			fflush(stdout); /* forces to empty the buffer, in other words, forces the printf to be shown; */
+			fgets (name, 15, stdin); /* gets the string of 15 characters typed by the user on the keyboard and puts it in the variable 'name'; */
+			while(error == false && i<strlen(name)-1){ /* while there is no error AND the variable 'i' is inferior to the length of the string in the variable 'name'-1; */
+				if((0<=name[i] && name[i]<=44) || (47<=name[i] && name[i]<=64) || (91<=name[i] && name[i]<=96) || (123<=name[i] && name[i]<=255)){ /* if the letter in the place 'i' of the variable 'name' isn't a letter (if the ascii number or the vharacter isn't a min, maj letter or a - and a. ); */
+					error = true; /* there is a error, so the variable 'error' is true; */
+					printf("There is a forbidden character in your name : %c\n",name[i]); /* the program tells the user the first forbidden character; */
+					strncpy(name,"",15); /* the program empties the variable 'string' */
+				}
+				i++; /* passing to the next letter; */
 			}
-			i++;
+			i = 0; /* new 'name', setting to the first letter; */
+			error = false; /* new 'name', setting the 'error' to false; */
 		}
-		i=0;
-		error=0;
-	}
 
-	fprintf(file,"%d,%s",score,name);
-	fclose(file);
+		name[strlen(name)-1] = 0; /* erasing the last character of 'name', who is a \n */
+
+		return name;
 }
 
 
@@ -63,54 +47,163 @@ void scoreWrite(int score){
  */
 int lineCount(){
 
-	FILE* file = fopen("highscore.txt","r");
-	int lines=0;
-	char chr;
+	FILE* file = fopen("highscore.txt","r"); /* opens the file highscore.txt and reads it */
+	int lines=0; /* initialize the lines integer to 0 */
+	char chr; /* initialize the char variable chr */
 
-	chr=getc(file);
-	while (chr != EOF){
+	chr=getc(file); /* chr gets the first char of the file */
+	while (chr != EOF){ /* while chr isn't the end of file:*/
 
-		 if (chr == '\n'){
-	            lines = lines + 1;
+		 if (chr == '\n'){ /*if ch is the character '\n':*/
+	            lines = lines + 1; /* there is a line, so add a line to lines;*/
 	     }
-		 chr = getc(file);
+		 chr = getc(file); /* chr gets the next char of the file */
 	}
-	fclose(file);
+	fclose(file); /* closes the file */
 	return lines;
 
 }
+
 
 
 /*
  * Function that creates an array of highscores
  * @return the pointer to the  array of scores
  */
- highscore* scoreArray(){
+ highscore* scoreArray(int size){
 
 	 FILE* file = fopen("highscore.txt","r");
-	 int size = lineCount(), score;
+	 int victories,loses;
 	 char username[15];
 
 	 highscore* scores = (highscore*) malloc(size * sizeof(highscore));
 
 	 for(int i=0;i<size;i++){
 
-		 fscanf(file,"%d,%s\n",&score,username);
-		 scores[i].score = score;
+		 fscanf(file,"%d,%d,%s\n",&victories,&loses,username);
+		 scores[i].victories = victories;
+		 scores[i].loses = loses;
 		 strcpy(scores[i].name, username);
 
 	 }
-
+	 fclose(file);
 
 	 return scores;
  }
 
 
+ /*
+  * Function that updates the score of a player
+  */
+ void scoreUpdate(highscore* scores, int size){
+
+ 	FILE* fileWrite = fopen("highscore.txt","w+");
+ 	int victories,loses;
+ 	char name[15];
+
+
+
+ 		for(int j=0; j<size; j++){
+ 			victories = scores[j].victories;
+ 			loses = scores[j].loses;
+ 			strcpy(name,scores[j].name);
+ 			fprintf(fileWrite,"%d,%d,%s\n",victories,loses,name);
+ 		}
+ 		fclose(fileWrite);
+ }
+
+
 /*
- * Does a thing but idk yet
+ * Gets and write the name and the score of the person in the file
+ * @param
  */
-void thing(){
+void scoreWrite(boolean victory){
+
+	/* creating a string of 15 characters and putting in the name checked thanks to the function playerCheck */
+	char name[15];
+	strcpy(name,playerCheck());
+
+	int Player = -1,victories=0,loses=0,size=lineCount();
+
+	FILE* file = fopen("highscore.txt","a");
+	highscore* scores;
+
+	scores = scoreArray(size);
 
 
+	/*creating the array and verifying if the player is already in the Array*/
+	for(int i=0; i<size; i++){
+		if(strcmp(scores[i].name, name)==0){
+			Player = i;
+			printf("%d",Player);
+			fflush(stdout);
+		}
+	}
 
+	if(Player!=-1){
+		if(victory == false){
+			scores[Player].loses++;
+		} else {
+			scores[Player].victories ++;
+		}
+		scoreSorting(scores,size);
+		scoreUpdate(scores,size);
+
+	} else {
+		if(victory == false){
+			loses++;
+		} else {
+			victories++;
+		}
+		fprintf(file,"%d,%d,%s\n",victories,loses,name);
+	}
+
+
+	fclose(file);
+}
+
+
+/*
+ * sort the scores from the highest to the lowest
+ * @param scores - array of highscores
+ * @param size - size of the array
+ */
+highscore* scoreSorting(highscore* scores, int size){
+
+	highscore* sortedArray = (highscore*) malloc(size * sizeof(highscore));
+
+	int victories, loses, position;
+	char name[15];
+
+	for(int i=1; i<size+1; i++){
+		position = i;
+		victories = scores[i].victories;
+		loses = scores[i].loses;
+		strcpy(name,scores[i].name);
+
+		for(int j=i-1 ;j>0 ; j--){
+			if(victories>scores[j].victories || (victories==scores[j].victories && loses<scores[j].loses)){
+				swap(scores,position,j);
+				position--;
+			}
+		}
+
+	}
+
+	sortedArray = scores;
+	return sortedArray;
+}
+
+/**
+ * Swap the two highscores at the indexes "index1" and "index2" in the array
+ * @param scores - a array of highscores
+ * @param index1 - an existing index in "myarray" to swap with "index2"
+ * @param index2 - an existing index in "myarray" to swap with "index2"
+ */
+void swap(highscore* scores, int index1, int index2) {
+	highscore swap;
+
+	swap = scores[index1];
+	scores[index1] = scores[index2];
+	scores[index2] = swap;
 }
--- Sources/highscore.h
+++ Sources/highscore.h
@@ -13,22 +13,24 @@
 #include <string.h>
 
 
-/* Creation of an Highscore type who takes 15 letters for the name of the user and then numbers for the score */
+/* Creation of structure composed of: */
 typedef struct {
-	int score;
-	char name[15];
+	int victories; /*an integer for the number of victories*/
+	int loses; /*an integer for the number of loses*/
+	char name[15]; /*a string for the name of the player*/
 } highscore;
 
+typedef enum {
+	false = 0,
+	true = 1
+} boolean;
 
 /*
- * Checks if the highscore.txt file is present, if not it creates it
+ * Gets the name typed by the user, verifies it.
+ * @return a string, the name of the user
  */
-void fileCheck();
+char* playerCheck();
 
-/*
- * Gets and write the name and the score of the person in the file
- */
-void scoreWrite(int score);
 
 /*
  * Function that counts the lines in the file
@@ -36,15 +38,42 @@ void scoreWrite(int score);
  */
 int lineCount();
 
+
 /*
  * Function that creates an array of highscores
  * @return the pointer to the  array of scores
  */
- highscore* scoreArray();
+ highscore* scoreArray(int size);
+
 
 /*
- * Does a thing but idk yet
+ * Function that updates the score of a player
  */
-void thing();
+void scoreUpdate(highscore* scores, int size);
+
+
+ /*
+  * Gets and write the name and the score of the person in the file
+  * @param
+  */
+ void scoreWrite(boolean victory);
+
+
+ /*
+  * sort the scores from the highest to the lowest
+  * @param scores - array of highscores
+  * @param size - sie of the array
+  */
+ highscore* scoreSorting(highscore* scores, int size);
+
+
+ /**
+  * Swap the two highscores at the indexes "index1" and "index2" in the array
+  * @param scores - a array of highscores
+  * @param index1 - an existing index in "myarray" to swap with "index2"
+  * @param index2 - an existing index in "myarray" to swap with "index2"
+  */
+ void swap(highscore* scores, int index1, int index2);
+
 
 #endif /* HIGHSCORE_HIGHSCORE_H_ */
