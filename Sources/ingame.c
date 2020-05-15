8110426db0bf0c7a1eaef835694182749ded9d30
QuinD3cim <58181290+QuinD3cim@users.noreply.github.com>
2020-05-15 15:50:49 +0200

    Adding function
    Single card print
    Beginning the player's hand card printing

--- Sources/ingame.c
+++ Sources/ingame.c
@@ -5,9 +5,31 @@
  */
 
 
+#include <ingame.h>
+
+
+/*
+ * Prints the cards in the player's hand
+ */
+void handPrint(Cards* Cards, int size){
+
+}
+
+
 /*
  * Function that prints a card
  */
-void cardPrint();
+void cardPrint(char number, char color){
+
+	if (color == 'H'){
+		printf(".---------.\n|%c.-----. |\n| : _ _ : |\n| :( V ): |\n| : \\ / : |\n| :  V  : |\n| '-----'%c|\n`---------'",number,number);
+	} else if (color == 'C'){
+		printf(".---------.\n|%c.-----. |\n| :  _  : |\n| : (_) : |\n| :( | ): |\n| : '^' : |\n| '-----'%c|\n`---------'",number,number);
+	} else if (color == 'S'){
+		printf(".---------.\n|%c.-----. |\n| :  ^  : |\n| : /_\\ : |\n| :(___): |\n| :  |  : |\n| '-----'%c|\n`---------'",number,number);
+	}else if (color == 'D'){
+		printf(".---------.\n|%c.-----. |\n| :  ^  : |\n| : / \\ : |\n| : \\ / : |\n| :  V  : |\n| '-----'%c|\n`---------'",number,number);
+	}
+}
 
 
--- Sources/ingame.h
+++ Sources/ingame.h
@@ -5,14 +5,32 @@
  */
 
 
-#ifndef INGAME_INGAME_H_
-#define INGAME_INGAME_H_
+#ifndef MAINMENU_INGAME_H_
+#define MAINMENU_INGAME_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    char value
+    char* color
+    int player
+    Boolean trump
+}Cards
+
+
+/*
+ * Prints the cards in the player's hand
+ */
+void handPrint(Cards* Cards, int size);
 
 
 /*
  * Function that prints a card
  */
-void cardPrint();
+void cardPrint(char number, char color);
 
 
-#endif /* INGAME_INGAME_H_ */
+#endif /* MAINMENU_INGAME_H_ */
