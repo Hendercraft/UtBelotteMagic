f3c3a1b741931b7867110ab842d3dba5cf46505f
QuinD3cim <58181290+QuinD3cim@users.noreply.github.com>
2020-05-15 14:19:28 +0200

    Debut of the ingame functions

---
+++ Sources/ingame.c
@@ -0,0 +1,13 @@
+/*
+ * InGame.c
+ *
+ *  Created by William Mann
+ */
+
+
+/*
+ * Function that prints a card
+ */
+void cardPrint();
+
+
---
+++ Sources/ingame.h
@@ -0,0 +1,18 @@
+/*
+ * InGame.h
+ *
+ *  Created by William Mann
+ */
+
+
+#ifndef INGAME_INGAME_H_
+#define INGAME_INGAME_H_
+
+
+/*
+ * Function that prints a card
+ */
+void cardPrint();
+
+
+#endif /* INGAME_INGAME_H_ */
